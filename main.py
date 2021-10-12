34ebd5aecc4b5c8283ccb5a0351ee6b6bf9d7aae
ucos <ucos4989@gmail.com>
2021-10-12 08:45:04 +0000

    Added validation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #FastAPI
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Query, Path
 
 app = FastAPI()
 
@@ -11,5 +11,14 @@ def read_root():
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
-    
 
+@app.get('/validation/{path}')
+async def validation(
+        string: str = Query(None, min_length=2, max_length=5, regex=r'[a-c]+.'),
+        integer: int = Query(..., gt=1, le=3),  # required
+        alias_query: str = Query('default', alias='alias-query'),
+        path: int = Path(10)):
+
+    return {"string": string, "integer": integer, "alias-query": alias_query, "path": path}
+
+#@app.get("/recommend")
